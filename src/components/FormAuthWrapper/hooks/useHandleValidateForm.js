8188899099460b30b69d5f85c0e0e72d7695a5f5
Mr. Punk Da Silva <gustavojs417@gmail.com>
2024-08-31 15:35:31 -0300

    criando a validação basica dos inputs

--- src/components/FormAuthWrapper/hooks/useHandleValidateForm.js
+++ src/components/FormAuthWrapper/hooks/useHandleValidateForm.js
@@ -1,6 +1,31 @@
-// TODO: CREATE HANDLE VALIDATE
-const useHandleValidateForm = () => {
+import SwalFire from "../../../utils/SwalFire.js";
 
+const useHandleValidateForm = ( formRef, typeAuth ) => {
+    const form = formRef;
+    const inputsForm = form.current.getElementsByTagName( "input" );
+    let inputsUsed = {
+        email : false,
+        password : false,
+        name : false,
+    };
+
+    inputsUsed.email = inputsForm.email;
+    inputsUsed.password = inputsForm.password;
+
+    // TODO: CHECK EMAIL AND PASSWORD IS EMPTY
+    if ( inputsForm.email.value === "" || inputsForm.email.value === "" ) {
+        SwalFire.error("Atenção", "Os campos são de preenchimento obrigatorio")
+    }
+
+    // TODO: VALIDATION REGISTER
+    if ( typeAuth === "register" ) {
+        inputsUsed.name = inputsForm.name;
+    }
+
+    // TODO: VALIDATION LOGIN
+    if ( typeAuth === "login" ) {
+
+    }
 }
 
 export default useHandleValidateForm;
