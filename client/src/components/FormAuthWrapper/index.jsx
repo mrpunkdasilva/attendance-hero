b63e1a9430eedc853e03a7ef3125c3ebd136fb45
Mr Punk da Silva <gustavojs417@gmail.com>
2025-03-04 17:20:12 -0300

    finalized auth pages

--- client/src/components/FormAuthWrapper/index.jsx
+++ client/src/components/FormAuthWrapper/index.jsx
@@ -1,104 +1,110 @@
-import { useRef } from 'react';
+import {useRef} from 'react';
 import "./styles/main.scss";
 import usePasswordVisibility from "./hooks/usePasswordVisibility.js";
 import useHandleValidateForm from "./hooks/useHandleValidateForm.js"
 
-const FormAuthWrapper = ( { title, typeAuth, action, handleFormAuth } ) => {
-    const [ iconPassword, showPassword, togglePasswordVisibility ] = usePasswordVisibility();
-    const formRef = useRef( null );
+const FormAuthWrapper = ({title, typeAuth, action, handleFormAuth}) => {
+    const [iconPassword, showPassword, togglePasswordVisibility] = usePasswordVisibility();
+    const formRef = useRef(null);
 
     return (
         <div className="form-auth-wrapper auth-login">
             <h1>
-                { title }
+                {title}
             </h1>
 
-            <div className={ "separator" }></div>
-
-            <p>
-                <small>
-                    * Todos os campos com asteriscos são obrigatórios.
-                </small>
-            </p>
+            <div className={"separator"}></div>
 
             <form
-                action={ action }
+                action={action}
                 className="form-control"
-                onSubmit={ () => handleFormAuth(formRef) }
-                ref={ formRef }
+                onSubmit={() => handleFormAuth(formRef)}
+                ref={formRef}
             >
-                {
-                    typeAuth === "register" && (
-                        <div className="form-group">
-                            <label htmlFor="name">* Nome</label>
+                <p>
+                    <small>
+                        * Todos os campos com asteriscos são obrigatórios.
+                    </small>
+                </p>
+
+                <div className={"container-wrapper"}>
+                    {
+                        typeAuth === "register" && (
+                            <div className="form-group">
+                                <label htmlFor="name">* Nome</label>
+                                <input
+                                    className="input-field"
+                                    type="text"
+                                    placeholder="Nome"
+                                    name="name"
+                                    required
+                                    pattern="[a-zA-Z\s]{5,}"
+                                    title="Por favor, informe um nome válido."
+                                />
+                            </div>
+                        )
+                    }
+                    <div className="form-group">
+                        <label htmlFor="name">* E-mail</label>
+                        <input
+                            className="input-field"
+                            type="email"
+                            placeholder="E-mail"
+                            name="email"
+                            required
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}"
+                            title="Por favor, informe um e-mail válido."
+                        />
+                    </div>
+                    <div className="form-group-password">
+                        <label htmlFor="name">* Password</label>
+                        <div className={"group"}>
                             <input
                                 className="input-field"
-                                type="text"
-                                placeholder="Nome"
-                                name="name"
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                placeholder="Senha"
                                 required
-                                pattern="[a-zA-Z\s]{5,}"
-                                title="Por favor, informe um nome válido."
+                                pattern="(?=.*[a-zA-Z])(?=.*\d).{8,}"
+                                title="Senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número."
                             />
+                            <button
+                                className="btn-visibility-password"
+                                type="button"
+                                onClick={togglePasswordVisibility}
+                            >
+                                <img
+                                    className="icon-interface"
+                                    src={iconPassword} alt="Icone de mostrar a senha"
+                                />
+                            </button>
                         </div>
-                    )
-                }
-                <div className="form-group">
-                    <label htmlFor="name">* E-mail</label>
-                    <input
-                        className="input-field"
-                        type="email"
-                        placeholder="E-mail"
-                        name="email"
-                        required
-                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}"
-                        title="Por favor, informe um e-mail válido."
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="name">* Password</label>
-                    <input
-                        className="input-field"
-                        type={ showPassword ? "text" : "password" }
-                        name="password"
-                        placeholder="Senha"
-                        required
-                        pattern="(?=.*[a-zA-Z])(?=.*\d).{8,}"
-                        title="Senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número."
-                    />
 
-                    <button
-                        className="btn-visibility-password"
-                        type="button"
-                        onClick={ togglePasswordVisibility }
-                    >
-                        <img
-                            className="icon-interface"
-                            src={ iconPassword } alt="Icone de mostrar a senha"
-                        />
-                    </button>
+                    </div>
+
                 </div>
 
                 <button
                     type="submit"
                     className="btn-auth-primary"
-                    disabled={ false }
-                    onClick={ (event) => {
+                    disabled={false}
+                    onClick={(event) => {
                         event.preventDefault();
-                        useHandleValidateForm( formRef, typeAuth )
-                    } }
+                        useHandleValidateForm(formRef, typeAuth)
+                    }}
                 >
-                    { typeAuth.toLowerCase() === "login" ? "Login" : "Sign up" }
+                    {typeAuth.toLowerCase() === "login" ? "Login" : "Sign up"}
                 </button>
 
-                <div className={ "separator" }></div>
+
+                <div className={"separator"}></div>
 
                 <a
-                    href={ `/${ typeAuth.toLowerCase() === "login"? "register" : "login" }` }
-                    type={ "button" }
-                    className={ "btn-auth-secondary" }
+                    href={`/${typeAuth.toLowerCase() === "login" ? "register" : "login"}`}
+                    type={"button"}
+                    className={"btn-auth-secondary"}
                 >
-                    { typeAuth.toLowerCase() === "login"? "Sign up" : "Login" }
+                    {typeAuth.toLowerCase() === "login" ? "Sign up" : "Login"}
                 </a>
             </form>
         </div>
